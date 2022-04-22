8a6f8cc1b544bb1e60b65d9fb0882964cba88e1c
Rupakkk <rupak.fcc@gmail.com>
2022-04-22 08:22:22 +0545

    cbv

--- myapp.py
+++ myapp.py
@@ -14,7 +14,7 @@ def get_id(id=None):
     data = r.json()
     print(data)
     
-# get_id()
+get_id()
 
 
 def post_create():
@@ -32,15 +32,26 @@ def post_create():
 
 def post_update():
     data = {
-        'id': 3,
-        'name': 'Ramhari',
-        'age' : 45,
-        'address': 'Lumbini'
+        'id': 4,
+        'name': 'harihari',
+        'age' : 65,
+        'address': 'butwal'
 
     }
     json_data = json.dumps(data)
-    r = requests.update(url= URL,data = json_data)
+    r = requests.put(url= URL,data = json_data)
     data = r.json()
     print(data)
 
 
+# post_update()
+
+def del_data(id):
+    data ={'id':id}
+    json_data = json.dumps(data)
+    r = requests.delete(url = URL, data = json_data)
+    data =  r.json()
+    print(data)
+# del_data(4)
+
+    
